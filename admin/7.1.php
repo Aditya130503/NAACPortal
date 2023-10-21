bd6f50c34cd4eabe385c4fe90bf36721ecfb6e23
Aditya <101393510+Aditya130503@users.noreply.github.com>
2023-10-21 11:36:34 +0530

    update

--- admin/7.1.php
+++ admin/7.1.php
@@ -42,20 +42,20 @@
 </head>
 <body>
     <div class="form-container">
+        <form action="ins.php" method="post" enctype="multipart/form-data">
         <h1>7.1 Institutional Values and Social Responsibilities</h1>
 
         <h2>7.1.1 Measures initiated by the Institution for the promotion of gender equity and Institutional
          initiatives to celebrate / organize national and international commemorative days, events and
          festivals during the last five years</h2>
-        <input type="text" id="vac" name="vac" required>
+        <input type="text" id="ge" name="ge" required>
 
-        <label for="add2">Upload Additional information </label><br>
-        <input type="file" id="add2" name="add2">
+        <label for="add45">Upload Additional information </label><br>
+        <input type="file" id="add45" name="add45">
 
-        <label for="link2">Provide link for additional information </label><br>
-        <input type="text" id="link2" name="link2">
+        <label for="link14">Provide link for additional information </label><br>
+        <input type="text" id="link14" name="link14">
 
-        <!-- Section 2.1.1 - Name and Address of the College -->
         <h3>7.1.2 The Institution has facilities and initiatives for
             1.Alternate sources of energy and energy conservation measures
             2.Management of the various types of degradable and nondegradable waste
@@ -63,9 +63,9 @@
             4.Green campus initiatives
             5.Disabled-friendly, barrier free environment
         </h3>
-        <input type="text" id="intern" name="intern" required>
-        <label for="add3"link3>Upload supporting document </label><br>
-        <input type="file" id="add3" name="add3">
+        <input type="text" id="faci" name="faci" required>
+        <label for="add46">Upload supporting document </label><br>
+        <input type="file" id="add46" name="add46">
 
         <h3>7.1.3 Quality audits on environment and energy regularly undertaken by the Institution. The
             institutional environment and energy initiatives are confirmed through the following
@@ -74,9 +74,9 @@
             3.Clean and green campus initiatives
             4.Beyond the campus environmental promotion activities
         </h3>
-        <input type="text" id="intern" name="intern" required>
-        <label for="add3"link3>Upload supporting document </label><br>
-        <input type="file" id="add3" name="add3">
+        <input type="text" id="qae" name="qae" required>
+        <label for="add47">Upload supporting document </label><br>
+        <input type="file" id="add47" name="add47">
 
 
         <h3>7.1.4 Describe the Institutional efforts/initiatives in providing an inclusive environment i.e., tolerance
@@ -85,16 +85,17 @@
             citizens (Within 500 words)
         </h3>
 
-        <input type="text" id="vac" name="vac" required   maxlength="500">
+        <textarea id="vrd" name="vrd" required   maxlength="500"></textarea>
 
-        <label for="add2">Upload Additional information </label><br>
-        <input type="file" id="add2" name="add2">
+        <label for="add48">Upload Additional information </label><br>
+        <input type="file" id="add48" name="add48">
 
-        <label for="link2">Provide link for additional information </label><br>
-        <input type="text" id="link2" name="link2">
+        <label for="link15">Provide link for additional information </label><br>
+        <input type="text" id="link15" name="link15">
 
 
-        <button formaction="currenrich.html">Submit</button>
+        <button name="submit27">Submit</button>
+    </form>
     </div>
 </body>
 </html>
