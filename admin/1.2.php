10b44d02a63252e1b011fdf4b3cf77b160943042
Abdullah9759 <144339244+Abdullah9759@users.noreply.github.com>
2023-10-24 00:09:07 +0530

    ok

--- admin/1.2.php
+++ admin/1.2.php
@@ -3,7 +3,7 @@
     include('style.css');
 ?>
 <html lang="en">
-<head>k
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>College Information Form</title>
